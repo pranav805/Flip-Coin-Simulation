c992a4a9f3ed02afed3d6339112785cec4facc5e
pranav805 <pranav.ige96@gmail.com>
2020-01-25 16:41:46 +0530

    ADDED: Calculated percentege of doublet calculation

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -1,22 +1,51 @@
-#!/bin/bash
+#!/bin/bash -x
 echo "Welcome to Flip Coin Simulation"
 declare -A coin
-read -p "Enter number of times you want to flip a coin " n
-headCount=0
-tailCount=0
-for(( i=0; i<$n; i++ ))
-do
-	randomCheck=$(( RANDOM%2 ))
-	if [ $randomCheck -eq 1 ]
-	then
-		coin[H]=$(( ++headCount ))
-	else
-		coin[T]=$(( ++tailCount ))
-	fi
-done
-echo Keys :	${!coin[@]}
-echo Count:	${coin[@]}
+read -p "Enter number of times you want to flip a coin " noOfFlips
+echo -e "1. Singlet Combination\n2. Doublet Combination"
+read -p "Enter your choice " choice
 
-echo Percentage of head: `echo "scale=2;${coin[H]}*100/$n" | bc`"%"
-echo Percentage of tail: `echo "scale=2;${coin[T]}*100/$n" | bc`"%"
+case $choice in
+   1) noOfCoins=1
+      ;;
+   2) noOfCoins=2
+      ;;
+   *) echo "Invalid choice!"
+      ;;
+esac
 
+function FlipCoin()
+{
+	for(( i=1; i<=$1; i++ ))
+	do
+		flipSide=""
+		for((j=1; j<=$2; j++))
+		do
+			randomCheck=$(( RANDOM%2 ))
+			if [ $randomCheck -eq 1 ]
+			then
+				flipSide+=H
+			else
+				flipSide+=T
+			fi
+		done
+		updateCount $flipSide
+	done
+}
+
+function updateCount()
+{
+	coin[$1]=$((${coin[$1]}+1))
+}
+
+function calculatePercentage()
+{
+	for i in ${!coin[@]}
+	do
+		coin[$i]=`echo "scale=2; ${coin[$i]}*100/$noOfFlips" | bc`
+	done
+	echo "Keys      : ${!coin[@]}"
+	echo "Percentage: ${coin[@]}"
+}
+FlipCoin $noOfFlips $noOfCoins
+calculatePercentage
